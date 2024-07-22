c5bb28a5a15bbcb3234edc2898973e64df344928
changha <mentenseoul@gmail.com>
2024-07-22 16:50:35 +0900

    init project

---
+++ potato-field/src/main/java/com/tools/potato_field/PotatoFieldApplication.java
@@ -0,0 +1,13 @@
+package com.tools.potato_field;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoFieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotatoFieldApplication.class, args);
+	}
+
+}
---
+++ potato-field/src/test/java/com/tools/potato_field/PotatoFieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tools.potato_field;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoFieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
