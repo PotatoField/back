7601c12ba115b1b175b9bfa6507b7f510f7fda10
hwi-hwi-hwi <164748897+hwi-hwi-hwi@users.noreply.github.com>
2024-07-22 17:54:40 +0900

    tets

--- potato-field/src/main/java/com/tools/potato_field/PotatoFieldApplication.java
+++ potato-field/src/main/java/com/tools/potato_field/PotatoFieldApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PotatoFieldApplication {
 
-	// dev_sh test
 	public static void main(String[] args) {
 		SpringApplication.run(PotatoFieldApplication.class, args);
 	}
