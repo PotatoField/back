f9445bd6313339225cdadcc2ae0950f81d1555b9
pepcsy <jjhjsy7@gmail.com>
2024-08-12 17:21:23 +0900

    test!!!

--- potato-field/src/main/java/com/tools/potato_field/likes/LikesRepository.java
+++ potato-field/src/main/java/com/tools/potato_field/likes/LikesRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
-//    Optional<Likeke> findByMemberIdAndPostId(Long memberId, Long postId);
+    Optional<Like> findByMemberIdAndPostId(Long memberId, Long postId);
 }
