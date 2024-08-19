38f178c87636bc770b0732b7dc2a503eff98b196
unknown <junyoung6215@gmail.com>
2024-08-19 16:36:26 +0900

    revised ver 0819 study

--- potato-field/src/main/java/com/tools/potato_field/WebConfig.java
+++ potato-field/src/main/java/com/tools/potato_field/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")  // 프론트엔드 서버 주소
+                .allowedOrigins("http://localhost:3000",
+                        "http://172.30.1.78:3000")  // 프론트엔드 서버 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
