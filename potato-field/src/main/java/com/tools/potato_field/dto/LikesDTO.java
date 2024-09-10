b5b59c5d30dcdb77862a23f1e9d408328ae80f52
pepcsy <jjhjsy7@gmail.com>
2024-09-10 21:58:14 +0900

    좋아요 ê°개수 조회 기능 추가

---
+++ potato-field/src/main/java/com/tools/potato_field/dto/LikesDTO.java
@@ -0,0 +1,13 @@
+package com.tools.potato_field.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LikesDTO {
+    private Long id;
+    private Long memberId;
+    private Long postId;
+}
+
