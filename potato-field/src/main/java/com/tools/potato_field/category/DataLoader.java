2b9cd26f9528469faa6121aead49f57a090fa813
hwi-hwi-hwi <164748897+hwi-hwi-hwi@users.noreply.github.com>
2024-10-02 12:44:59 +0900

    카테고리 및 데이터 초기화 관련 코드 수정

--- potato-field/src/main/java/com/tools/potato_field/category/DataLoader.java
+++ potato-field/src/main/java/com/tools/potato_field/category/DataLoader.java
@@ -1,2 +1,35 @@
-package com.tools.potato_field.category;public class DataLoader {
+package com.tools.potato_field.category;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class DataLoader {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @PostConstruct
+    public void loadData() {
+        // location 하위 항목
+        List<String> locations = Arrays.asList("공릉", "성수", "이태원", "홍대", "강남", "여의도");
+        // TPO 하위 항목
+        List<String> tpos = Arrays.asList("데일리", "데이트", "캠퍼스", "출근", "운동");
+
+        // location 항목 저장
+        locations.forEach(location -> {
+            Category locationCategory = new Category(location, "location");
+            categoryRepository.save(locationCategory);
+        });
+
+        // TPO 항목 저장
+        tpos.forEach(tpo -> {
+            Category tpoCategory = new Category(tpo, "TPO");
+            categoryRepository.save(tpoCategory);
+        });
+    }
 }
