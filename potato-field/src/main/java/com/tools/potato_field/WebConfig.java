771438e1af0ac781da10bdf9bdb24d64773e632d
hwi-hwi-hwi <venusksh224@gmail.com>
2024-12-20 16:21:54 +0900

    서버 연결  해결

--- potato-field/src/main/java/com/tools/potato_field/WebConfig.java
+++ potato-field/src/main/java/com/tools/potato_field/WebConfig.java
@@ -10,8 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000",
-                        "http://10.50.44.159:3000")  // 프론트엔드 서버 주소
+                .allowedOrigins(
+                        "http://localhost:3000",       // 로컬 개발 환경
+                        "http://10.50.44.159:3000",    // 테스트 환경
+                        "http://52.79.100.109"         // 배포된 프론트엔드 주소
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
